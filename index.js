bc1081c4a32a19024c88f961443064ac3aaf9596
K1tanaK0mbat <kkeayouth@gmail.com>
2023-09-18 13:15:11 -0500

    created questions

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generate = require('./utils/generateMarkdown.js')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        title:"What should be the title of your project?",
+        Description:"Provide a description for your project:purpose,lesson,goal",
+        Installation: "Provide Instructions for installation",
+        Usage: "Explain how to use application",
+        Contributing:"Tell other users how to contribute to continued development",
+        Tests:"Test Instructions"
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
