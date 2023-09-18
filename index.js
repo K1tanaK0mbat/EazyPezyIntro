fee7cad6404ade0ebb277c0a93e9fec73e416d7c
K1tanaK0mbat <kkeayouth@gmail.com>
2023-09-18 13:28:42 -0500

    Used array and object for question code

--- index.js
+++ index.js
@@ -1,24 +1,56 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generate = require('./utils/generateMarkdown.js')
+//const generate = require('./utils/generateMarkdown.js')
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-        title:"What should be the title of your project?",
-        Description:"Provide a description for your project:purpose,lesson,goal",
-        Installation: "Provide Instructions for installation",
-        Usage: "Explain how to use application",
-        Contributing:"Tell other users how to contribute to continued development",
-        Tests:"Test Instructions"
-    }
+        "What should be the title of your project?",
+        "Provide a description for your project:purpose,lesson,goal",
+         "Provide Instructions for installation",
+         "Explain how to use application",
+        "Tell other users how to contribute to continued development",
+        "Test Instructions"
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const [title, description, installation, usage, contribution, test]=questions;
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() 
+{inquirer
+.prompt([
+{
+    type:'input',
+    name:'title',
+    message:title,
+},
+{
+    type:'input',
+    name:'describe',
+    message:description,
+},
+{
+    type:'input',
+    name:'install',
+    message:installation,
+},
+{
+    type:'input',
+    name:'usage',
+    message:usage,
+},
+{
+    type:'input',
+    name:'contribute',
+    message:contribution,
+},
+{
+    type:'input',
+    name:'test',
+    message:test,
+},
+])
+};
 
 // Function call to initialize app
 init();
