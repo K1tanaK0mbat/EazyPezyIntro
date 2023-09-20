c6c1b96dce68bf76275c186e9b61e13cc7df4eea
K1tanaK0mbat <kkeayouth@gmail.com>
2023-09-20 07:44:50 -0500

    added function for license links

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,19 @@
+const license = data.license;
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+var link = "";
+    switch (license){
+    case 'MIT':
+         return link='choosealicense.com/licenses/mit';
+    break;    
+    case 'ISC':
+        return link='https://choosealicense.com/licenses/isc/';
+        break;
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
